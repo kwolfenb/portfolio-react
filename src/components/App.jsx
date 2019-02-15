8c26c42c1a7e97fa69821978e28840b926427856
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2019-02-15 10:15:52 -0800

    initialize project files with completed page layout file

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import { Switch, Route } from 'react-router-dom'
+// import PropTypes from 'prop-types'
+
+
+function App () {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+      <style jsx> {`
+        .header {
+          background-color: gray;
+        }
+      `}
+      </style>
+      <h1>
+        Kenny Wolfenberger Project Portfolio
+      </h1>
+    </div>
+  )
+}
+
+export default Header

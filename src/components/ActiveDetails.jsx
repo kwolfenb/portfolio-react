31b4ce295cf53230776cfd16d38cc1c9ce6eae1a
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2019-02-22 15:18:24 -0800

    update styling

--- src/components/ActiveDetails.jsx
+++ src/components/ActiveDetails.jsx
@@ -7,6 +7,7 @@ class ActiveDetails extends React.Component {
     super(props)
     this.state = {
       imgPreview: 'projImg',
+      closeButton: 'none'
     }
     this.previewImg = this.previewImg.bind(this)
   }
@@ -15,10 +16,12 @@ class ActiveDetails extends React.Component {
     if (this.state.imgPreview == 'projImg') {
       this.setState({
         imgPreview: 'projImgLarge',
+        closeButton: 'initial'
       })
     } else if (this.state.imgPreview == 'projImgLarge') {
       this.setState({
         imgPreview: 'projImg',
+        closeButton: 'none'
       })
     }
   }
@@ -54,6 +57,7 @@ class ActiveDetails extends React.Component {
             <div className='grid-item2'>
               <h4>Preview:</h4>
               <img className={this.state.imgPreview} src={this.props.activeDetails.img} onClick={this.previewImg} />
+              <h5 id='close' onClick={this.previewImg} >Click to close</h5>
             </div>
           </div>
         </div>
@@ -102,15 +106,15 @@ class ActiveDetails extends React.Component {
         img.projImgLarge {
           position: fixed;
           z-index: 2;
-          left: 17.5%;
-          top: 5%;
-          width: 65%;
+          left: 15%;
+          top: 2%;
+          width: 70%;
           height: auto;
-          max-height: 90%;
-          overflow: auto;
-          border: solid blue;
-        }
+          max-height: 95%;
+          border: solid gray 2px;
+          box-shadow: 0 0 30px darkblue;
 
+        }
         img.projImg:hover {
           cursor: zoom-in;
           box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.2), 0 2px 8px 0 rgba(0, 0, 0, 0.19);
@@ -121,6 +125,27 @@ class ActiveDetails extends React.Component {
           font-size: 16px;
         }
 
+        #close {
+          display: ${this.state.closeButton};
+          position: fixed;
+          text-align: center;
+          z-index: 3;
+          bottom: 5%;
+          border: solid black 2px;
+          padding: 6px;
+          left: auto;
+          background: white;
+          font-size: 18px;
+          background-color: #ad1111;
+          color: white;
+          font-weight: bolder;
+          text-decoration: none;
+        }
+        #close:hover {
+          cursor: pointer;
+          border: solid gray 2px;
+          background-color: red;
+        }
     `}
         </style>
         <div >

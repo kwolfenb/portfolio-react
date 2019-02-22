1e9ca8fa6465380f23908ffc0b64357dd6cdb862
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2019-02-22 11:49:48 -0800

    add state functionality to ProjectList to dynamically view project details

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Link} from 'react-router-dom'
 
 function ProjectDetails (props) {
-  let expandedDetails =
-  
-    <div>
-      <img src={props.img} />
-    </div>
     
   return (
     <div>
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,25 +1,61 @@
 import React from 'react'
 import ProjectDetails from './ProjectDetails'
 import proj1 from '../assets/images/web_clone_img.jpg'
+import proj2 from '../assets/images/type_img.jpg'
+import proj3 from '../assets/images/salon.jpg'
+import proj4 from '../assets/images/storefinder.jpg'
+import proj5 from '../assets/images/tamagotchi.jpg'
+import proj6 from '../assets/images/age.jpg'
 
 class ProjectList extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      activeDetails: {}
+      activeDetails: null
     }
     this.handleShowDetails = this.handleShowDetails.bind(this)
   }
 
-  handleShowDetails(proj) {
-    console.log(proj)
+  handleShowDetails(id) {
+    let activeProject = Projects[id]
     this.setState({
-      activeDetails: proj
+      activeDetails: activeProject
     })
   }
 
   render() {
+
+    let projectDetails = null
+    if (this.state.activeDetails) {
+      projectDetails =
+        <div className='activeDetails'>
+          <h3>Project Name: {this.state.activeDetails.projectName}</h3>
+          <h4>Description:  {this.state.activeDetails.expandedDetails}</h4>
+          <div className='grid-container2'>
+            <div className='grid-item2'>
+              <h4>Technologies Used: </h4>
+              <ul>
+                {this.state.activeDetails.technologies.map((x) => {
+                  return (
+                    <div>
+                      <li>{x}</li>
+                    </div>
+                  )
+                })}
+              </ul>
+            </div>
+            <div className='grid-item2'>
+              <h4>Preview</h4>
+              <img className='projImg' src={this.state.activeDetails.img} />
+            </div>
+            <div className='grid-item2'>
+              <h4>Readme</h4>
+              <img className='projImg' src={this.state.activeDetails.img} />
+            </div>
+          </div>
+        </div>
+    }
 
     return (
       <div>
@@ -42,6 +78,28 @@ class ProjectList extends React.Component {
         .grid-item:hover {
           background: linear-gradient(#dbe7ff, white);
         }
+
+        .activeDetails {
+          border: solid black;
+          width: 75%;
+          margin: auto;
+          padding: 5px;
+        }
+
+        .grid-container2 {
+          display: grid;
+          grid-template-columns: auto auto auto;
+        }
+
+        .grid-item2 ul {
+          list-style: none;
+        }
+
+        img.projImg{
+          width: 350px;
+          height: auto;
+        }
+
       `}
         </style>
         <div className='grid-container'>
@@ -58,6 +116,10 @@ class ProjectList extends React.Component {
             </div>
           )}
         </div>
+        <hr />
+        <div>
+          {projectDetails}
+        </div>
       </div>
     )
   }
@@ -67,45 +129,51 @@ export default ProjectList
 
 const Projects = [
   {
-    projectName: 'Hair Salon',
-    info: 'A C# ASP/.NET application that is connected to a SQL database with tables for Hair Stylists and Clients',
-    link: 'https://github.com/kwolfenb/HairSalon.Solution',
+    projectName: 'Website Clone',
+    info: 'Clone of the Eddie Bauer website using TypeScript and Angular.',
+    link: 'https://github.com/kwolfenb/Website-Clone-Angular',
     img: proj1,
-    showDetails: false,
+    expandedDetails: 'A C# application that is connected to a SQL database with tables for Stylists and Clients. Users can add to the Client lists for each individual Stylist. Any time a Client is added, it must be associated with a Stylist. Stylist table includes details such as name, address, phone number and Client list.',
+    technologies: ['C#', 'ASP/.Net', 'MSTests', 'MVC', 'Razor', 'Mono', 'SQL',],
   },
   {
     projectName: 'Type Racer',
     info: 'This JavaScript application is an online typing tool which allows people to race against the computer by typing various quotes.',
     link: 'https://github.com/kwolfenb/typing-game-angular',
-    img: proj1,
-    showDetails: false,
+    img: proj2,
+    expandedDetails: 'This is an online typing competition, TypeRacer, allows people to race against the computer by typing various quotes from books, movies, and songs. Uses Firebase to store history of games as well as database for quotes to type.',
+    technologies: ['TypeScript', 'Node.JS', 'Firebase Authentication', 'Firebase Realtime Database', 'node packet manager', 'Angular 5', 'HTML/CSS', 'jQuery']
+  },
+  {
+    projectName: 'Hair Salon',
+    info: 'A C# ASP/.NET application that is connected to a SQL database with tables for Hair Stylists and Clients',
+    link: 'https://github.com/kwolfenb/HairSalon.Solution',
+    img: proj3,
+    expandedDetails: 'A C# application that is connected to a SQL database with tables for Stylists and Clients. Users can add to the Client lists for each individual Stylist. Any time a Client is added, it must be associated with a Stylist. Stylist table includes details such as name, address, phone number and Client list.',
+    technologies: ['C#', 'ASP/.Net', 'MSTests', 'MVC', 'Razor', 'Mono', 'SQL',]
   },
   {
     projectName: 'Store Finder',
     info: 'C# ASP/.NET application where users can search Database for stores by Location and Name.',
     link: 'https://github.com/kwolfenb/StoreFinder.Solution',
-    img: proj1,
-    showDetails: false,
+    img: proj4,
+    expandedDetails: 'Allows users to search Database for Dispensaries by Location and Name by using Web API to a map/GPS service. Database can be updated through a web scraper that runs Yelp searches and pulls information for stores within the Database.',
+    technologies: ['C#', 'ASP/.Net Core MVC', 'Selenium WebDriver', 'SQL', 'Here Maps API', 'JavaScript', 'Razor']
   },
   {
     projectName: 'Tamagotchi',
-    info: 'Users can search Database for stores by Location and Name.',
+    info: 'Users can interact with a Tamagotchi pet in this JavaScript-based web application.',
     link: 'https://github.com/kwolfenb/tamagotchi-js-webpack',
-    img: proj1,
-    showDetails: false,
-  },
-  {
-    projectName: 'Website Clone',
-    info: 'Clone of the Eddie Bauer website using TypeScript and Angular.',
-    link: 'https://github.com/kwolfenb/Website-Clone-Angular',
-    img: proj1,
-    showDetails: false,
+    img: proj5,
+    expandedDetails: 'User can interact with Tamagotchi in many different ways: feed, exercise, give Gameboy, give pills, harvest gems, and wrestle opponents.  Interactions have effects on Tamagotchi\'s health, food level and happiness levels. When wrestling opponents, the program will simulate moves from each player based on stats and will determine winner. Game is won if Tamagotchi achieves age of "Geezer"',
+    technologies: ['C#', 'ASP/.Net', 'MSTests', 'MVC', 'Razor', 'Mono', 'SQL',]
   },
   {
     projectName: 'Age Calculator',
     info: 'JavaScript application that calculates a user\'s age on other planets based on inputted date.',
     link: 'https://github.com/kwolfenb/age-calculator-webpack-js',
-    img: proj1,
-    showDetails: false,
+    img: proj6,
+    expandedDetails: 'JavaScript application that calculates a user\'s age on other planets based on inputted date. Users can interact with the application by entering a birth date and clicking on planets.',
+    technologies: ['JavaScript', 'Node Package Manager (NPM)', 'JSON', 'Webpack', 'ESLint', 'Jasmine/Karma testing', 'Babel',]
   }
 ]
